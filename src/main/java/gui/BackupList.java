0296519fb04806aa307d08d9ad9539b194be1f83
yav <796176@protonmail.com>
2024-06-01 20:37:57 +0300

    Extent the BackupList public interface
    The BackupList interface provides the ability to manipulate its items.

--- src/main/java/gui/BackupList.java
+++ src/main/java/gui/BackupList.java
@@ -5,18 +5,65 @@ import constants.Dimensions;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class BackupList extends JScrollPane {
-	private final BackupItem[] backupItems;
+	private BackupItem[] backupItems = new BackupItem[]{};
 	public BackupList() {
 		super(VERTICAL_SCROLLBAR_ALWAYS, HORIZONTAL_SCROLLBAR_NEVER);
 
-		JPanel innerPanel = new JPanel();
-		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
-		innerPanel.setBackground(Colors.LAYER1);
-		setViewportView(innerPanel);
 		setPreferredSize(getMinimumSize());
 		verticalScrollBar.setUnitIncrement(Dimensions.DEFAULT_PANEL_SCROLL);
+		initiateList();
+	}
+
+	private JPanel getPanel(){
+		JPanel panel = new JPanel();
+		panel.setBackground(Colors.LAYER1);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+		return panel;
+	}
+
+	private void initiateList() {
+		JPanel panel = getPanel();
+		for (BackupItem backupItem: backupItems) {
+			panel.add(backupItem);
+			if (backupItem != backupItems[backupItems.length - 1]){
+				panel.add(Box.createRigidArea(new Dimension(0, Dimensions.DEFAULT_COMPONENT_OFFSET)));
+			}
+		}
+		setViewportView(panel);
+	}
+
+	public void addBackupItem(String name, int chainNumber, boolean bashScript, String date) {
+		assert name != null && chainNumber > -1 && date != null;
+
+		backupItems = Arrays.copyOf(backupItems, backupItems.length + 1);
+		backupItems[backupItems.length - 1] = new BackupItem(this, name, chainNumber, bashScript, date);
+		initiateList();
+	}
+
+	public void removeBackupItem(String name, int chainNumber, boolean bashScript, String date) {
+		assert name != null && chainNumber > -1 && date != null;
+
+		int index = 0;
+		BackupItem backupItem = new BackupItem(this, name, chainNumber, bashScript, date);
+		while (backupItems[index] != backupItem && ++index < backupItems.length);
+		if (index == backupItems.length) return;
+		System.arraycopy(backupItems, index + 1, backupItems, index, backupItems.length - 1 - index);
+		backupItems = Arrays.copyOf(backupItems, backupItems.length - 1);
+		initiateList();
+	}
+
+	public void changeBackupItem(BackupItem oldBI, String name, int chainNumber, boolean bashScript, String date) {
+		assert name != null && chainNumber > -1 && date != null;
+
+		int index = 0;
+		while (backupItems[index] != oldBI && ++index < backupItems.length);
+		if (index == backupItems.length) return;
+		backupItems[index] = new BackupItem(this, name, chainNumber, bashScript, date);
+		initiateList();
 	}
 
 	public BackupItem[] getBackupItems() {
